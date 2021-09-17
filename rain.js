770812c3ff6c5d2e4dc67367a685c68252afeeb8
kest1798 <john@kestwest.com>
2021-09-17 15:35:21 -0500

    Changed rain.js to support duet, added new colors to navigator.js

--- rain.js
+++ rain.js
@@ -2,116 +2,106 @@
 let cx; // canvas position
 let speed = -2;
 let score_len = 0;
-let segLen = 2;
-let genCount = 0;
-let hPos = 0;
-let vPos = 0;
-let proll;
 let sroll = 1;
 let opacity = 3;
-let wait = 0;
-let sNoise = 0;
 let lineCount = 0;
-let vals = [];
-let redone = [255,255,255];
-let n_redone = [255,255,255];
-let depth = 0;
+let rainTeam = [];
 const WMOD = 1;
 
 function setup() {
     frameRate(60);
     w = windowWidth * WMOD;
     h = windowHeight * WMOD;
-    hPos = random(w);
-    // hPos = w/2;
     createCanvas(w, h);
-    sNoise = h;
     cx = w;
     pixelDensity(1);
-    setSegmentLength();
-    ++lineCount;
+
+    rainTeam[0] = new RainStreak(0);
+    rainTeam[1] = new RainStreak(1);
 }
 
 function draw() {
-    if (wait == 0) {
-        background(0, opacity);
-        if (opacity > 3) {
-            opacity *= 0.99;
-        }
-        // Slow down thicker lines (segLen)
-        if (frameCount % Math.ceil(segLen * 0.25) == 0) {
-            drawGenerator(1, 0);
-        }
-    } else {
-        --wait;
-        if (wait == 0) {
-            strokeWeight(sroll);
-        }
+    background(0, opacity);
+    if (opacity > 3) {
+        opacity *= 0.99;
+    }
+    // Slow down thicker lines (segLen)
+    // console.log(" Math.ceil(rainTeam[0].segLen * 0.25): "+Math.ceil(rainTeam[0].segLen * 0.25));
+    if (frameCount % Math.ceil(rainTeam[0].segLen * 0.25) == 0) {
+        rainTeam[0].display();
+        // console.log("rainTeam[0].vPos: "+rainTeam[0].vPos+" rainTeam[0].rolling: "+rainTeam[0].rolling);
+    }
+    if (frameCount % Math.ceil(rainTeam[1].segLen * 0.25) == 0) {
+        rainTeam[1].display();
     }
 }
 
-function setSegmentLength() {
-    let nextNoise = h;
-    // chance of noise increases with lineCount
-    // First five will not have noise
-    if (rollDice(lineCount) > 5) {
-        // randomize noise starting height
-        nextNoise = random(h * 0.125, h);
+class RainStreak {
+    constructor(team) {
+        let s = rollDice(12);
+        this.team = team;
+        this.m = 0;
+        this.rolling = true;
+        this.vPos = 0;
+        this.hPos = random(w);
+        this.vals = [];
+        this.noise = 0;
+        this.sNoise = 0;
+        this.depth = 0;
+        this.sroll = s;
+        this.segLen = s;
+        this.sNoise = h;
     }
-    n_redone = [255,255,255];
-    if (rollDice(16) == 1) {
-        n_redone =  [255,0,0];
+    newRainStreak() {
+        let nextNoise = h;
+
+        // chance of noise increases with lineCount
+        // First five will not have noise
+        if (rollDice(lineCount) > 5) {
+            // randomize noise starting height
+            nextNoise = random(h * 0.125, h);
+        }
+        return [rollDice(12), nextNoise, random(w)];
     }
-    return [rollDice(12), nextNoise, random(w), n_redone];
-}
+    display() {
+        this.rolling = true;
+        while (this.rolling) {
+            if (this.vPos == 0) {
+                this.vals = this.newRainStreak();
+            }
+            this.noise = 0;
+            if (this.vPos > this.sNoise) {
+                this.depth = this.vPos - this.sNoise;
+                this.noise = round(random(this.depth * -0.025, this.depth * 0.025));
+                // console.log("noise: "+this.noise+" depth: "+this.depth);
+            }
+            // draw pre stroke
+            strokeWeight(this.vals[0]);
+            stroke(chooseTeam(this.team));
+            line(this.vals[2], 0, this.vals[2], this.vals[0]);
 
-function drawGenerator(spd, y_offset) {
-    ++genCount;
-    let m = 0;
-    let rolling = true;
-    let roll;
+            // draw current stroke
+            strokeWeight(this.sroll);
+            stroke(chooseTeam(this.team));
+            line(this.hPos, this.vPos, this.hPos + this.noise, this.vPos + this.segLen);
 
-    // draw horizontal line downward
-    while (rolling) {
-        if (vPos == 0) {
-            vals = setSegmentLength();
+            // lower the line and add noise
+            this.vPos += this.segLen;
+            this.hPos += this.noise;
+            // console.log("this.vPos: "+this.vPos+ " this.segLen: "+this.segLen+" h: "+h);
+            // if the line is done
+            if (this.vPos >= h) {
+                console.log("lineCount: "+lineCount+" this.sroll: "+this.sroll);
+                this.sroll = this.vals[0];
+                this.segLen = this.vals[0];
+                this.sNoise = this.vals[1];
+                this.hPos = this.vals[2];
+                this.vPos = 0;
+                this.depth = 0;
+                ++lineCount;
+            }
+            this.rolling = false;
         }
-        let noise = 0;
-        if (vPos > sNoise) {
-        // if (true) {
-            depth = vPos - sNoise;
-            // depth++;
-            noise = round(random(depth * -0.025, depth * 0.025));
-            console.log("noise: "+noise+" depth: "+depth);
-        }
-        // draw pre stroke
-        strokeWeight(vals[0]);
-        stroke(n_redone);
-        line(vals[2], 0, vals[2], vals[0]);
-
-        // draw current stroke
-        strokeWeight(sroll);
-        stroke(redone);
-        line(hPos, vPos, hPos + noise, vPos + segLen);
-
-        // lower the line and add noise
-        vPos += segLen;
-        hPos += noise;
-
-        // if the line is done
-        if (vPos >= h) {
-            // setSegmentLength();
-            sroll = vals[0];
-            segLen = vals[0];
-            sNoise = vals[1];
-            hPos = vals[2];
-            redone = vals[3];
-            ++lineCount;
-            vPos = 0;
-            depth = 0;
-        }
-        rolling = false;
-        proll = roll;
     }
 }
 
